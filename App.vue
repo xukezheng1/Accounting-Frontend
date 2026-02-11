2fec02833932e7da7fe92a04d7f4a58fba592e15
xkz <1444010162@qq.com>
2026-02-11 15:33:42 +0800

    初始化

---
+++ App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch');
+  }
+};
+</script>
+
+<style lang="scss">
+@import './uni.scss';
+page {
+  background: #f4f6f8;
+}
+</style>
---
+++ main.js
@@ -0,0 +1,12 @@
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+import uviewPlus from 'uview-plus';
+import 'uview-plus/index.scss';
+
+export function createApp() {
+  const app = createSSRApp(App);
+  app.use(uviewPlus);
+  return {
+    app
+  };
+}
