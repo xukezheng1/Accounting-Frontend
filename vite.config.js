9a4718b7bf315fba771f0532d3769bbf03ff772a
xkz <1444010162@qq.com>
2026-02-12 17:32:25 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [uni()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "uview-plus/theme.scss";`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './')
